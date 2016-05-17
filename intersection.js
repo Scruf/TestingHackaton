0f4761abba801b389b327083bde9476507291290
Egor <egor.kozitski@mail.citytech.cuny.edu>
2016-05-17 16:39:17 -0400

    Big file reading implemented

--- intersection.js
+++ intersection.js
@@ -5,25 +5,37 @@ var mongoose = require('mongoose'),
     fs = require('fs'),
     xml2json = require('xml2js'),
     Street = require('./Street'),
+    uril = require('util'),
+    stream = require('stream'),
+    es = require('event-stream'),
     osmium = require('osmtogeojson'),
     mongodbURI = "mongodb://ek5442:NokiaLumia920@ds033875.mongolab.com:33875/movies";
 mongoose.connect(mongodbURI);
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
-osmium('C:/Users/ekozi/Downloads/NewYork.osm_01.osm');
+var file_path ='C:/Users/ekozi/Downloads/NewYork.osm_01.osm';
 
-// var parser = new xml2json.Parser(function(result,error){
-//     if(error)
-//         throw error;
-//
-// });
-//
-// fs.readFile('C:/Users/ekozi/Downloads/NewYork.osm_01.osm',function (err,data) {
-//     if(parser.parseString(data)){
-//         console.log('successfuly parsed file');
-//     }else{
-//         console.error("Error parsing file");
-//     }
-//
-// });
-//
+var line_number = 0 ;
+var s = fs.createReadStream(file_path)
+    .pipe(es.split())
+    .pipe(es.mapSync(function(line){
+
+            // pause the readstream
+            s.pause();
+
+            line_number += 1;
+
+            // process line here and call s.resume() when rdy
+            // function below was for logging memory usage
+            console.log(line);
+
+            // resume the readstream, possibly from a callback
+            s.resume();
+        })
+        .on('error', function(){
+            console.log('Error while reading file.');
+        })
+        .on('end', function(){
+            console.log('Read entire file.')
+        })
+    );
