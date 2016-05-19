227599a37fbcd381e83e60639fd19182ed5fdc50
Egor <egor.kozitski@mail.citytech.cuny.edu>
2016-05-18 20:31:27 -0400

    Added json file for new york

--- geojson_pusher.js
+++ geojson_pusher.js
@@ -4,27 +4,43 @@
 var fs = require('fs'),
     Street = require('./Street'),
     mongoose = require('mongoose'),
+    request = require('request'),
     mongodbURI = "mongodb://ek5442:NokiaLumia920@ds033875.mongolab.com:33875/movies";
 mongoose.connect(mongodbURI);
 var db = mongoose.connection;
 db.on('error',console.error.bind(console,'connection error'));
+request('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nyzd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=json',function(err,response,body){
 
-fs.readdir('C:/Users/ekozi/Downloads/geojson-master/geojson-master',function(err,data){
-    if(err)
+    if (err){
         throw err;
-    else{
-        data.map(function(el){
-            return el.split(".")[1]==='geojson';
-        })
-        fs.readFile('C:/Users/ekozi/Downloads/geojson-master/geojson-master/'+data[1],'utf8',function(err,data){
-            if(err)
-                throw err;
-           else{
-                var coordinate_obj = JSON.parse(data);
-                
-                console.log(coordinate_obj.features)
+    }
 
-            }
+    else {
+        console.log(body)
+        fs.writeFile('new_york.json',body,function(err,data){
+            if (err)
+                throw err;
+            else
+                console.log(data);
         })
     }
-})
+
+});
+// fs.readdir('C:/Users/ekozi/Downloads/geojson-master/geojson-master',function(err,data){
+//     if(err)
+//         throw err;
+//     else{
+//         data.map(function(el){
+//             return el.split(".")[1]==='geojson';
+//         })
+//         fs.readFile('C:/Users/ekozi/Downloads/geojson-master/geojson-master/'+data[1],'utf8',function(err,data){
+//             if(err)
+//                 throw err;
+//            else{
+//                 var coordinate_obj = JSON.parse(data);
+//
+//                 console.log(coordinate_obj)
+//
+//             }
+//         })
+//     }
